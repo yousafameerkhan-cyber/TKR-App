b5cd77ec811460b632f8df3130f0067843406894
yousafameerkhan-cyber <yousafameerkhan.official@gmail.com>
2026-08-12 16:01:07 +0500

    Initial commit - PWA features added

---
+++ public/sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'tkr-cache-v1';
+const urlsToCache = [
+  '/customer.html',
+  '/logo.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
